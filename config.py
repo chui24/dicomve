068cc5e495b834326d2a36ad05e5eef9eb3e52b2
jmachado <jesusmachadoalbarran29@gmail.com>
2024-06-07 14:53:33 -0400

    Ajustando la definicion de la base de datos creada.

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/postgres/dicomve'
-app.config['SECRET_KEY'] = 'your_secret_key'  # Necesario para Flask-Login
-db = SQLAlchemy(app)
+db = "postgresql://dicomve:dicomve@localhost:5432/dicomve"
+
